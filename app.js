4f469edb69a871f76e94b2b4b259a2354c63e7df
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-02-16 12:57:53 +0100

    added new solution

--- app.js
+++ app.js
@@ -161,3 +161,13 @@ const gimme = (triplet) => {
 };
 
 // console.log(gimme([2,3,1]))
+
+//! Is this a triangle
+
+const isTriangle = (a, b, c) => {
+  if (a <= 0 || b <= 0 || c <= 0) return false;
+  if (a + b <= c || a + c <= b || b + c <= a) return false;
+  return true;
+};
+
+// console.log(isTriangle(1,2,2))
