306e32dc808b23d896ba7f332ed90e5152683630
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-02-23 11:28:20 +0100

    added challenge

--- app.js
+++ app.js
@@ -298,3 +298,26 @@ const solution = (string) => {
 }
 
 // console.log(solution("camelCasingTestCase")); 
+
+
+//! Sum a list but ignore any duplicates
+
+const sumNoDuplicates = (numList) => {
+  let counts = {};
+
+  for (let i = 0; i < numList.length; i++) {
+    let num = numList[i];
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+
+  let sum = 0;
+  for (let num in counts) {
+    if(counts[num] === 1) {
+      sum += Number(num);
+    }
+  }
+
+  return sum;
+}
+
+// console.log(sumNoDuplicates([0, 10, 8, 9, 7, 3, 3, 9, 3, 6, 0]))
