321c85f137383a8d591905778ea608eacdec1644
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-02-23 10:32:42 +0100

    added challenge

--- app.js
+++ app.js
@@ -276,3 +276,25 @@ const revWords =  (str) => {
 // console.log(revWords("double  spaced  words"))
 
 
+//! Break camelCase
+
+const solution = (string) => {
+    let words = string.split("");
+    let newStr = "";
+
+    words.forEach(word => {
+      if( word === word.toUpperCase()) {
+        newStr += " " + word;
+      } else {
+        newStr += word
+      }
+    })
+
+
+  return newStr;
+
+  //? Done with Regex
+  // return(string.replace(/([A-Z])/g, ' $1'));
+}
+
+// console.log(solution("camelCasingTestCase")); 
