8b40a1fc639591e03c5e28ad33f4251f00c2425e
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-02-28 14:05:35 +0100

    duplicate encoder 6kyu

---
+++ 6_kyu/duplicate_encoder.js
@@ -0,0 +1,14 @@
+const duplicateEncode = (word) => {
+    let encoder = "";
+    word = word.toLowerCase();
+    for (let i = 0; i < word.length; i++) {
+        if(word.indexOf(word[i]) === word.lastIndexOf(word[i])) {
+            encoder += "(";
+        } else{
+            encoder += ")";
+        }
+    }
+
+    return encoder;
+}
+
