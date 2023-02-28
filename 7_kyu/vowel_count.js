ccdf6cbd83a4ec094e83ca19803ba9116d6b543e
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-02-28 13:08:11 +0100

    vowel count 7kyu

---
+++ 7_kyu/vowel_count.js
@@ -0,0 +1,12 @@
+const getCount = (str) => {
+    let count = 0;
+    let vowels = ["a","e","i","o","u"];
+
+    for (let i = 0; i < str.length; i++) {
+        if(vowels.includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
