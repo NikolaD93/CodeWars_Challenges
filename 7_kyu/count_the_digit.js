257d531cee1d136d80f9f846b464764a3d058c59
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-03-01 11:13:17 +0100

    count the digits 7kyu

---
+++ 7_kyu/count_the_digit.js
@@ -0,0 +1,16 @@
+const nbDig = (n, d) => {
+  let counter = 0;
+
+  for (let i = 0; i <= n; i++) {
+    let multiply = i * i;
+    multiply = String(multiply).split("");
+    for (let k = 0; k < multiply.length; k++) {
+      if(Number(multiply[k]) === d) counter++;
+    }
+  }
+
+  return counter;
+};
+
+
+
