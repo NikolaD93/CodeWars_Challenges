0bbba98871149ca67dd29886dc35274e55a3ffc3
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-03-02 14:22:51 +0100

    all unique 7kyu

---
+++ 7_kyu/all_unique.js
@@ -0,0 +1,15 @@
+const hasUniqueChars = (str) => {
+
+  //! Simple solution using Set  
+  // return new Set(str).size === str.length
+
+  //! My solution
+  str = str.split("");
+
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i]) !== i) return false;
+  }
+  return true;
+};
+
+
